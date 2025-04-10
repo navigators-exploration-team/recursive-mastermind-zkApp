f80c562779e97a12ea1fb79a7eb4fe1311274aca
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2025-04-10 16:49:52 +0300

    feat: replace all util functions with new Combination and Clue classes, remove maxAttempt from GameState

--- src/utils.ts
+++ src/utils.ts
@@ -1,348 +1,182 @@
 import { Field, Bool, Provable, UInt64, UInt32, Struct, UInt8 } from 'o1js';
+import { MAX_ATTEMPTS } from './Mastermind';
 
-export {
-  separateCombinationDigits,
-  compressCombinationDigits,
-  validateCombination,
-  serializeClue,
-  deserializeClue,
-  serializeClueHistory,
-  deserializeClueHistory,
-  getClueFromGuess,
-  checkIfSolved,
-  serializeCombinationHistory,
-  deserializeCombinationHistory,
-  updateElementAtIndex,
-  getElementAtIndex,
-  GameState,
-};
+export { Combination, Clue, GameState };
 
-/**
- * Separates a four-digit Field value into its individual digits.
- *
- * @param combination - The four-digit Field to be separated.
- * @returns An array of four Field digits representing the separated digits.
- *
- * @throws Will throw an error if the combination is not a four-digit number.
- *
- * @note The function first asserts that the input is a valid four-digit Field.
- *       The digits are then witnessed, and their correctness is asserted by re-compressing
- *       them back into the original combination and ensuring equality.
- */
-function separateCombinationDigits(combination: Field) {
-  // Assert that the combination is a four-digit Field
-  const isFourDigit = combination
-    .greaterThanOrEqual(1000)
-    .and(combination.lessThanOrEqual(9999));
-  isFourDigit.assertTrue('The combination must be a four-digit Field!');
-
-  // Witness single digits of the combination
-  const digits = Provable.witness(Provable.Array(Field, 4), () => {
-    const num = combination.toBigInt();
-    return [num / 1000n, (num / 100n) % 10n, (num / 10n) % 10n, num % 10n];
-  });
-
-  // Assert the correctness of the witnessed digit separation
-  compressCombinationDigits(digits).assertEquals(combination);
-
-  return digits;
-}
-
-/**
- * Combines an array of four digits into a single Field value.
- *
- * @note An additional check to ensure that the input has exactly four digits would typically be necessary.
- * However, since this function is primarily used within {@link separateCombinationDigits}, the input is
- * already validated as a four-digit Field array by `Provable.Array(Field, 4)`, which inherently ensures the array has a length of 4.
- *
- * @param combinationDigits - An array of four Field digits.
- * @returns The combined Field element representing the original four-digit number.
- */
-function compressCombinationDigits(combinationDigits: Field[]) {
-  return combinationDigits[0]
-    .mul(1000)
-    .add(combinationDigits[1].mul(100))
-    .add(combinationDigits[2].mul(10))
-    .add(combinationDigits[3]);
-}
-
-/**
- * Validates the combination digits to ensure they meet the game rules.
- *
- * @param combinationDigits - An array of four Field digits representing the combination.
- *
- * @throws Will throw an error if any digit (except the first) is 0 or if any digits are not unique.
- *
- * @note The first digit is not checked for 0 because it would reduce the combination to a 3-digit value.
- *       The combination digits are provided by {@link separateCombinationDigits}, which ensures they form
- *       a valid four-digit number.
- */
-function validateCombination(combinationDigits: Field[]) {
-  for (let i = 1; i < 4; i++) {
-    // Ensure the digit is not zero (only for digits 2, 3, and 4)
-    combinationDigits[i]
-      .equals(0)
-      .assertFalse(`Combination digit ${i + 1} should not be zero!`);
-
-    // Ensure the digits are unique
-    for (let j = i; j < 4; j++) {
-      combinationDigits[i - 1].assertNotEquals(
-        combinationDigits[j],
-        `Combination digit ${j + 1} is not unique!`
-      );
+class Combination extends Struct({
+  digits: Provable.Array(Field, 4),
+}) {
+  static from(numbers: number[]) {
+    if (numbers.length !== 4) {
+      throw new Error('Combination must have exactly 4 digits');
     }
+    return new this({
+      digits: numbers.map((number) => Field(number)),
+    });
   }
-}
 
-/**
- * Serializes an array of Field elements representing a clue into a single Field
- * Each clue element is converted to 2 bits and then combined into a single dField.
- *
- * @param clue - An array of 4 Field elements, each representing a part of the clue.
- * @returns - A single Field representing the serialized clue.
- */
-function serializeClue(clue: Field[]): Field {
-  const clueBits = clue.map((f) => f.toBits(2)).flat();
-  const serializedClue = Field.fromBits(clueBits);
+  toBits() {
+    return this.digits.map((digit) => digit.toBits(3)).flat();
+  }
 
-  return serializedClue;
-}
+  compress() {
+    return Field.fromBits(this.toBits());
+  }
 
-/**
- * Deserializes a Field into an array of Field elements, each representing a part of the clue.
- * The serialized clue is split into 2-bit segments to retrieve the original clue elements.
- *
- * @note This function is not used within a zkApp itself but is utilized for reading and deserializing
- * on-chain stored data, as well as verifying integrity during integration tests.
- *
- * @param serializedClue - A Field representing the serialized clue.
- * @returns - An array of 4 Field elements representing the deserialized clue.
- */
-function deserializeClue(serializedClue: Field): Field[] {
-  const bits = serializedClue.toBits(8);
-  const clueA = Field.fromBits(bits.slice(0, 2));
-  const clueB = Field.fromBits(bits.slice(2, 4));
-  const clueC = Field.fromBits(bits.slice(4, 6));
-  const clueD = Field.fromBits(bits.slice(6, 8));
+  static decompress(compressedCombination: Field) {
+    const bits = compressedCombination.toBits(12);
 
-  return [clueA, clueB, clueC, clueD];
-}
+    return new this({
+      digits: [
+        Field.fromBits(bits.slice(0, 3)),
+        Field.fromBits(bits.slice(3, 6)),
+        Field.fromBits(bits.slice(6, 9)),
+        Field.fromBits(bits.slice(9, 12)),
+      ],
+    });
+  }
 
-/**
- * Compares the guess with the solution and returns a clue indicating hits and blows.
- * A "hit" is when a guess digit matches a solution digit in both value and position.
- * A "blow" is when a guess digit matches a solution digit in value but not position.
- *
- * @param guess - The array representing the guessed combination.
- * @param solution - The array representing the correct solution.
- * @returns - An array where each element represents the clue for a corresponding guess digit.
- *                           2 indicates a "hit" and 1 indicates a "blow".
- */
-function getClueFromGuess(guess: Field[], solution: Field[]) {
-  let clue = Array.from({ length: 4 }, () => Field(0));
+  validate() {
+    for (let i = 1; i < 4; i++) {
+      this.digits[i]
+        .equals(0)
+        .or(this.digits[i].equals(8))
+        .or(this.digits[i].equals(9))
+        .assertFalse(`Combination digit ${i + 1} is not in range [1, 7]!`);
 
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      const isEqual = guess[i].equals(solution[j]).toField();
-      if (i === j) {
-        clue[i] = clue[i].add(isEqual.mul(2)); // 2 for a hit (correct digit and position)
-      } else {
-        clue[i] = clue[i].add(isEqual); // 1 for a blow (correct digit, wrong position)
+      for (let j = i; j < 4; j++) {
+        this.digits[i - 1].assertNotEquals(
+          this.digits[j],
+          `Combination digit ${j + 1} is not unique!`
+        );
       }
     }
   }
 
-  return clue;
-}
+  private static decompressHistory(compressedHistory: Field) {
+    const historyBits = compressedHistory.toBits(12 * MAX_ATTEMPTS);
+    const historyBitPacks: Bool[][] = [];
 
-/**
- * Determines if the secret combination is solved based on the given clue.
- *
- * @param clue - An array representing the clues for each guess.
- * @returns Returns true if all clues indicate a "hit" (2), meaning the secret is solved.
- */
-function checkIfSolved(clue: Field[]) {
-  let isSolved = Bool(true);
+    for (let i = 0; i < historyBits.length; i += 12) {
+      historyBitPacks.push(historyBits.slice(i, i + 12));
+    }
 
-  for (let i = 0; i < 4; i++) {
-    let isHit = clue[i].equals(2);
-    isSolved = isSolved.and(isHit);
+    return historyBitPacks.map((bits) => Field.fromBits(bits));
   }
 
-  return isSolved;
-}
+  static updateHistory(
+    newCombination: Combination,
+    compressedHistory: Field,
+    index: Field
+  ) {
+    const combinationHistory = this.decompressHistory(compressedHistory);
 
-/**
- * Serializes an array of clues into a single `Field` by converting each clue into an 8-bit representation.
- *
- * @param clues - An array of `Field` elements representing the clues.
- * @returns - A serialized `Field` containing the bitwise representation of the clue history.
- */
-function serializeClueHistory(clues: Field[]): Field {
-  return serialize(clues, 8);
-}
+    const newCombinationCompressed = newCombination.compress();
 
-/**
- * Deserializes a `Field` back into an array of clues, where each clue is 8 bits long.
- *
- * @param serializedClueHistory - A `Field` containing the serialized clue history.
- * @returns - The deserialized array of `Field` elements representing the clues.
- */
-function deserializeClueHistory(serializedClueHistory: Field): Field[] {
-  // 120 bits total, each clue is 8 bits
-  return deserialize(serializedClueHistory, 120, 8);
-}
+    for (let i = 0; i < MAX_ATTEMPTS; i++) {
+      combinationHistory[i] = Provable.if(
+        index.equals(i),
+        newCombinationCompressed,
+        combinationHistory[i]
+      );
+    }
 
-/**
- * Serializes an array of combinations into a single `Field` by converting each combination into 14 bits.
- *
- * @param combinations - The array of `Field` elements representing combinations.
- * @returns - The serialized `Field` containing the combination history.
- */
-function serializeCombinationHistory(combinations: Field[]): Field {
-  return serialize(combinations, 14);
-}
-
-/**
- * Deserializes a `Field` back into an array of combinations, where each combination is 14 bits long.
- *
- * @param serializedCombinationHistory - A `Field` containing the serialized combination history.
- * @returns - The deserialized array of `Field` elements representing the combinations.
- */
-function deserializeCombinationHistory(
-  serializedCombinationHistory: Field
-): Field[] {
-  // 210 bits total, each combination is 14 bits
-  return deserialize(serializedCombinationHistory, 210, 14);
-}
-
-/**
- * Retrieves the `Field` element at a specified index from an array of `Field` elements.
- * Ensures that only one element matches the provided index and throws an error if none or multiple match.
- *
- * @param fieldArray - An array of `Field` elements.
- * @param index - The index of the element to retrieve as a `Field`.
- * @returns - The `Field` element at the specified index.
- * @throws Will throw an error if the index is out of bounds or if multiple indices match.
- */
-function getElementAtIndex(fieldArray: Field[], index: Field): Field {
-  const length = fieldArray.length;
-  let totalIndexMatch = Field(0);
-  let selectedValue = Field(0);
-
-  // Iterate through the array and match the element at the given index
-  for (let i = 0; i < length; i++) {
-    const isMatch = index.equals(Field(i)).toField(); // `1` if index matches, otherwise `0`
-    const matchingValue = isMatch.mul(fieldArray[i]); // Retain value only if index matches
-
-    selectedValue = selectedValue.add(matchingValue); // Accumulate the matching value
-    totalIndexMatch = totalIndexMatch.add(isMatch); // Track if exactly one index matched
+    const updatedHistory = combinationHistory.map((c) => c.toBits(12)).flat();
+    return Field.fromBits(updatedHistory);
   }
 
-  // Ensure that exactly one index matched
-  const errorMessage =
-    'Invalid index: Index out of bounds or multiple indices match!';
-  totalIndexMatch.assertEquals(1, errorMessage);
+  static getElementFromHistory(compressedHistory: Field, index: Field) {
+    let combinationHistory = this.decompressHistory(compressedHistory);
 
-  return selectedValue; // Return the selected value
+    let element = Field(0);
+    for (let i = 0; i < MAX_ATTEMPTS; i++) {
+      element = Provable.if(index.equals(i), combinationHistory[i], element);
+    }
+    return Combination.decompress(element);
+  }
 }
 
-/**
- * Updates an array of `Field` elements at a specified index with a new value.
- * Ensures the index is within bounds and updates only the specified element, while retaining all others.
- *
- * @param newValue - The new `Field` value to insert at the specified index.
- * @param fieldArray - The current array of `Field` elements.
- * @param index - The index at which to update the array.
- * @returns - The updated array of `Field` elements with the new value at the specified index.
- * @throws Will throw an error if the index is out of bounds.
- */
-function updateElementAtIndex(
-  newValue: Field,
-  fieldArray: Field[],
-  index: Field
-): Field[] {
-  // Ensure that the index is within bounds
-  const errorMessage = 'Invalid index: Index out of bounds!';
-  index.assertLessThan(fieldArray.length, errorMessage);
-
-  let updatedFieldArray: Field[] = [];
-
-  // Iterate through the array and update the element at the specified index
-  for (let i = 0; i < fieldArray.length; i++) {
-    updatedFieldArray[i] = Provable.if(
-      index.equals(i), // If current index matches the target index
-      newValue, // Update with the new value
-      fieldArray[i] // Otherwise, retain the original value
-    );
+class Clue extends Struct({
+  hits: Field,
+  blows: Field,
+}) {
+  compress() {
+    return Field.fromBits(this.hits.toBits(3).concat(this.blows.toBits(3)));
   }
 
-  return updatedFieldArray;
-}
-
-/**
- * Serializes an array of `Field` elements by converting each element into its bit representation
- * and flattening the resulting bit arrays into a single `Field`.
- *
- * @param fields - The array of `Field` elements to serialize.
- * @param range - The number of bits for each `Field` element.
- * @returns - A single `Field` containing the serialized bit representation of the array.
- */
-function serialize(fields: Field[], range: number): Field {
-  const bits = fields.map((c) => c.toBits(range));
-  return Field.fromBits(bits.flat());
-}
-
-/**
- * Deserializes a `Field` into an array of `Field` elements by splitting its bit representation
- * into chunks and converting them back into `Field` elements.
- *
- * @param serializedField - The serialized `Field` to deserialize.
- * @param size - The total number of bits in the serialized `Field`.
- * @param chunkSize - The bit size of each individual element in the array.
- * @returns - An array of deserialized `Field` elements.
- */
-function deserialize(
-  serializedField: Field,
-  size: number,
-  chunkSize: number
-): Field[] {
-  const packedBits = serializedField.toBits(size);
-  const unpackedBits: Bool[][] = [];
-
-  // Slice the bit representation into smaller arrays of length `chunkSize`
-  for (let i = 0; i < packedBits.length; i += chunkSize) {
-    const chunk = packedBits.slice(i, i + chunkSize);
-    unpackedBits.push(chunk);
+  static decompress(compressedClue: Field) {
+    const bits = compressedClue.toBits(6);
+    return new this({
+      hits: Field.fromBits(bits.slice(0, 3)),
+      blows: Field.fromBits(bits.slice(3, 6)),
+    });
   }
 
-  const unpacked = unpackedBits.map((bits) => Field.fromBits(bits));
-  return unpacked;
+  static giveClue(guess: Field[], solution: Field[]) {
+    let hits = Field(0);
+    let blows = Field(0);
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        const isEqual = guess[i].equals(solution[j]).toField();
+        if (i === j) {
+          hits = hits.add(isEqual);
+        } else {
+          blows = blows.add(isEqual);
+        }
+      }
+    }
+    return new this({ hits, blows });
+  }
+
+  isSolved() {
+    return this.hits.equals(4);
+  }
+
+  static updateHistory(newClue: Clue, compressedHistory: Field, index: Field) {
+    const historyBits = compressedHistory.toBits(6 * MAX_ATTEMPTS);
+    const historyBitPacks: Bool[][] = [];
+
+    for (let i = 0; i < historyBits.length; i += 6) {
+      historyBitPacks.push(historyBits.slice(i, i + 6));
+    }
+
+    let clueHistory = historyBitPacks.map((bits) => Field.fromBits(bits));
+
+    const newClueCompressed = newClue.compress();
+
+    for (let i = 0; i < MAX_ATTEMPTS; i++) {
+      clueHistory[i] = Provable.if(
+        index.equals(i),
+        newClueCompressed,
+        clueHistory[i]
+      );
+    }
+
+    const updatedHistory = clueHistory.map((c) => c.toBits(6)).flat();
+    return Field.fromBits(updatedHistory);
+  }
 }
 
 class GameState extends Struct({
   rewardAmount: UInt64,
   finalizeSlot: UInt32,
-  maxAttempts: UInt8,
   turnCount: UInt8,
   isSolved: Bool,
 }) {
   static default = new this({
     rewardAmount: UInt64.from(1e9),
     finalizeSlot: UInt32.from(0),
-    maxAttempts: UInt8.from(4),
     turnCount: UInt8.from(0),
     isSolved: Bool(false),
   });
 
   pack() {
-    const { rewardAmount, finalizeSlot, maxAttempts, turnCount, isSolved } =
-      this;
+    const { rewardAmount, finalizeSlot, turnCount, isSolved } = this;
 
     const serializedState = [
       rewardAmount.toBits(),
       finalizeSlot.toBits(),
-      maxAttempts.toBits(),
       turnCount.toBits(),
       isSolved.toField().toBits(1),
     ].flat();
@@ -355,14 +189,12 @@ class GameState extends Struct({
 
     const rewardAmount = UInt64.fromBits(bits.slice(0, 64));
     const finalizeSlot = UInt32.fromBits(bits.slice(64, 96));
-    const maxAttempts = UInt8.fromBits(bits.slice(96, 104));
-    const turnCount = UInt8.fromBits(bits.slice(104, 112));
-    const isSolved = bits[112];
+    const turnCount = UInt8.fromBits(bits.slice(96, 104));
+    const isSolved = bits[104];
 
     return new this({
       rewardAmount,
       finalizeSlot,
-      maxAttempts,
       turnCount,
       isSolved,
     });
